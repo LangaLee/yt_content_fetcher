77238decd7d3336fee9d0c609e8de0db38504b08
LangaLee <langa4749@gmail.com>
2025-09-24 15:37:11 +0000

    adds channel saving
    adds auto update from saved channels: last five videos
    
    adds grabbing multiple channels at once

--- src/yt_downloader/main.py
+++ src/yt_downloader/main.py
@@ -1,11 +1,16 @@
 import yt_dlp
+import questionary
 
 
-def fetch_video_list(channel_url):
+with open("../../subs.txt","r") as f:
+    usual_suspects = f.read().split("\n")
 
+
+def download_multiple_videos(channels,end):
+    urls = [f"{chan}/videos" for chan in channels]
     ydl_options = {
         "playliststart": 1,
-        "playlistend": 1,
+        "playlistend": end,
         "format": "bestvideo+bestaudio",
         "outtmpl": "downloads/%(title)s.%(ext)s",
         "download_archive": "downloaded.txt",
@@ -13,7 +18,7 @@ def fetch_video_list(channel_url):
     }
 
     with yt_dlp.YoutubeDL(ydl_options) as ydl:
-        ydl.download(channel_url)
+        ydl.download(urls)
 
 
 
@@ -24,10 +29,39 @@ def download_video(url):
 
     ydl_options = {
         "format": "bestvideo+bestaudio",
-        "outtmpl": info.get("title")
+        "outtmpl": f"downloads/{info.get("title")}"
     }
     with yt_dlp.YoutubeDL(ydl_options) as ydl:
         ydl.download(url)
 
-fetch_video_list("https://www.youtube.com/playlist?list=UUuYLDxZWv-aal88czKkFvKA")
-# download_video("https://youtu.be/aimvwkuhCIw?si=nmfnAmIcIJnaUNPK")
+
+def update_library():
+    choice = questionary.select(
+        "Donwload multiple or single",
+        choices=["single", "multiple","update"]
+    ).ask()
+    
+    yt_link = input("please enter video or channel link here: \n")
+
+
+    # making sure that we just get the pure channel link so we can save it later
+    if yt_link.endswith("/videos"):
+        yt_link = f"{yt_link[:yt_link.rfind("/videos")]}\n"
+
+    if choice == "single":
+        download_video(yt_link)
+    elif choice == "multiple":
+        end = input("num of videos: \n")
+        download_multiple_videos([yt_link],int(end))
+        if yt_link in usual_suspects:
+            print("channel already recorded")
+            return
+        with open("../../subs.txt","a") as f:
+            f.write(yt_link)
+        
+            
+    elif choice == "update":
+        download_multiple_videos(usual_suspects,5)
+
+#https://www.youtube.com/playlist?list=UUuYLDxZWv-aal88czKkFvKA
+update_library()
